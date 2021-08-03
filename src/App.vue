1b6b8d30febbf6670e97bb8813189b990029171d
Corina CIOBANU <cociobanu@pentalog.com>
2021-08-03 13:29:21 +0300

    added vuetify template and divided into components

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app id="inspire">
+    <NavBar/>
+    <v-main class="grey lighten-3">
+      <v-container>
+        <v-row>
+          <Categories/>
+          <v-col>
+            <v-sheet
+                min-height="70vh"
+                rounded="lg"
+            >
+              <!--  -->
+            </v-sheet>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import NavBar from "./components/NavBar";
+import Categories from "./components/Categories";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: {Categories, NavBar},
+  data: () => ({
+
+  }),
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-app-bar
+      app
+      color="white"
+      flat
+  >
+    <v-container class="py-0 fill-height">
+      <v-avatar
+          class="mr-10"
+          color="grey darken-1"
+          size="32"
+      ></v-avatar>
+
+      <v-btn
+          v-for="link in links"
+          :key="link"
+          text
+      >
+        {{ link }}
+      </v-btn>
+
+      <v-spacer></v-spacer>
+
+      <v-responsive max-width="260">
+        <v-text-field
+            dense
+            flat
+            hide-details
+            rounded
+            solo-inverted
+        ></v-text-field>
+      </v-responsive>
+      <i class="fas fa-shopping-cart fa-2x"></i>
+    </v-container>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+  data: () => ({
+    links: [
+      'Dashboard',
+      'Messages',
+      'Profile',
+      'Updates',
+    ],
+  }),
+}
+</script>
+
+<style scoped>
+
+</style>
