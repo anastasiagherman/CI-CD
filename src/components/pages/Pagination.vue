0bf904fec0cc33e31ab56c798e8a40bd1b8f381a
Corina CIOBANU <cociobanu@pentalog.com>
2021-08-05 13:37:48 +0300

    splited products into components

---
+++ src/components/pages/Pagination.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="text-center mb-3">
+    <v-pagination
+      v-model="page"
+      :length="6"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Pagination"
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/components/pages/Products.vue
+++ src/components/pages/Products.vue
@@ -12,71 +12,22 @@
           :key="n"
           cols="4"
         >
-          <v-card
-            :loading="loading"
-            class="mx-auto my-12"
-            max-width="374"
-          >
-            <template slot="progress">
-              <v-progress-linear
-                color="deep-purple"
-                height="10"
-                indeterminate
-              />
-            </template>
-
-            <v-img
-              height="250"
-              src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
-            />
-
-            <v-card-title>Cafe Badilico</v-card-title>
-
-            <v-card-text>
-              <v-row
-                align="center"
-                class="mx-0"
-              >
-                <v-rating
-                  :value="4.5"
-                  color="amber"
-                  dense
-                  half-increments
-                  readonly
-                  size="14"
-                />
-
-                <div class="grey--text ms-4">
-                  4.5 (413)
-                </div>
-              </v-row>
-
-              <div class="my-4 text-subtitle-1">
-                $ • Italian, Cafe
-              </div>
-
-              <div>Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio seating.</div>
-            </v-card-text>
-
-            <v-divider class="mx-4" />
-            <v-card-actions>
-              <v-btn
-                color="deep-purple lighten-2"
-                text
-              >
-                Add to cart
-              </v-btn>
-            </v-card-actions>
-          </v-card>
+          <ProductsItem />
         </v-col>
+      </v-row>
+      <v-row class="justify-center">
+        <Pagination />
       </v-row>
     </v-container>
   </v-main>
 </template>
 
 <script>
+import Pagination from "./Pagination";
+import ProductsItem from "./ProductsItem";
 export default {
 name: "Products",
+  components: {ProductsItem, Pagination},
   data: () => ({
     loading: false,
     selection: 1,
